907cb7a735201e056e3155c672b752050c289dd3
NamHoangVu <nam.hoang.vu03@gmail.com>
2025-10-21 12:48:44 +0200

    Still trying to deploy it on Github Pages

--- slots-ui/vite.config.js
+++ slots-ui/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Viktig: base må matche repo-navnet ditt på GitHub
 export default defineConfig({
   plugins: [react()],
+  base: '/Slots/',
 })
